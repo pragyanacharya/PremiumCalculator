658f8d1fc7eeb7ce4be5ec875e1ada3b00173984
patipragyan <patipragyan@gmail.com>
2021-08-16 00:50:29 +1000

    Repository Tests

---
+++ RepositoryTests/Queries/GetOccupations/GetOccupationsQueryTests.cs
@@ -0,0 +1,32 @@
+﻿using PremiumCalculator.API.Repository.Queries.GetOccupations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RepositoryTests.Queries.GetOccupations
+{
+    public class GetOccupationsQueryTests
+    {
+        private GetOccupationsQuery query;
+        private GetOccupationsQuery.Handler _handler;
+        private static readonly CancellationToken CancellationToken = CancellationToken.None;
+        public GetOccupationsQueryTests()
+        {
+            query = new GetOccupationsQuery();
+            _handler = new GetOccupationsQuery.Handler();
+        }
+
+        [Fact]
+        public async Task Should_Not_Return_Empty()
+        {
+            // Act
+            var result = await _handler.Handle(query, CancellationToken);
+
+            // Assert  
+            Assert.NotEmpty(result);
+        }
+    }
+}
