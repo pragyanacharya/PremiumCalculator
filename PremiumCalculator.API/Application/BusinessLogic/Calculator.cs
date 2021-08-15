29cad912f20fa8dfaa7febe3b454812883910ecd
patipragyan <patipragyan@gmail.com>
2021-08-15 19:35:47 +1000

    Refactoring

--- PremiumCalculator.API/Application/BusinessLogic/Calculator.cs
+++ PremiumCalculator.API/Application/BusinessLogic/Calculator.cs
@@ -1,11 +1,13 @@
-﻿namespace PremiumCalculator.API.Application.BusinessLogic
+﻿using System;
+
+namespace PremiumCalculator.API.Application.BusinessLogic
 {
     public static class Calculator
     {
         public static decimal Calculate(decimal sumInsured, decimal ratingFactor,int age)
         {
             var premium = (sumInsured * ratingFactor * age) / 1000 * 12;
-            return premium;
+            return Math.Round(premium, 2);
         }
     }
 }
